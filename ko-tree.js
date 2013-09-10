c8abd3a18c457959b59fddcf525c7aa76329c742
Steven Vergenz <vergenzs@gmail.com>
2013-09-10 13:09:34 -0400

    Completed dict nestable

--- ko-tree.js
+++ ko-tree.js
@@ -11,12 +11,12 @@
 
 		else if( type == 'object' )
 		{
-			console.log('Target is object');
 			target.c = {};
 			var result = ko.computed({
 
 				'read': function(){
-					return ko.toJS(target.c);
+					target();
+					return target.c;
 				},
 
 				'write': function(newval)
@@ -26,7 +26,6 @@
 					{
 						// update existing entries
 						if( target.c.hasOwnProperty(i) ){
-							console.log('Updating property', i);
 							target.c[i](newval[i]);
 						}
 
@@ -35,23 +34,21 @@
 						{
 							// create new child array
 							if( newval[i] instanceof Array ){
-								console.log('Creating array property', i);
 								target.c[i] = ko.observable(newval[i]).extend({'nested': 'array'});
 							}
 
 							// create new child object
 							else if( newval[i] instanceof Object ){
-								console.log('Creating object property', i);
 								target.c[i] = ko.observable(newval[i]).extend({'nested': 'object'});
 							}
 
 							// create new primitive observable
 							else {
-								console.log('Creating primitive property', i);
 								target.c[i] = ko.observable(newval[i]);
 							}
 
 							// subscribe parent to child updates
+							target.c[i].humanName = i;
 							target.c[i].subscribe(function(){
 								target.valueHasMutated();
 							});
@@ -61,7 +58,6 @@
 			});
 
 			result(target());
-			console.log('Done');
 			return result;
 		}
 
